1abba84f744c977eac9c79f7ef57a64910e96c8e
Gabriel Rosas <gabrielrosass@gmail.com>
2015-06-22 10:07:57 -0300

    construtores

---
+++ examples/config/database.php
@@ -0,0 +1,28 @@
+<?
+
+//self::environment('development', function(){
+//  self::database('primary', [
+//    'adapter' => 'mysql',
+//    'host' => 'localhost',
+//    'user' => 'root',
+//    'password' => '',
+//    'database' => 'teste1'
+//  ]);
+//  self::database('secundary', [
+//    'adapter' => 'mysql',
+//    'host' => 'localhost',
+//    'user' => 'root',
+//    'password' => '',
+//    'database' => 'database2'
+//  ]);
+//});
+
+//OR
+
+//self::environment('development', [
+//    'adapter' => 'mysql',
+//    'host' => 'localhost',
+//    'user' => 'root',
+//    'password' => '',
+//    'database' => 'database'
+//]);
